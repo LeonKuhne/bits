9820df0fee19bb24c3c5fcfc595d09977ed23eae
Leon K <leonkuhne@gmail.com>
2024-08-05 15:54:05 -0500

    cleaned up composer and path imports

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 class Component extends HTMLElement {
   static tag = 'fill-elem';
-  static create(name, data) {
+  static create(path, data) {
     const component = document.createElement(Component.tag)
-    component.setAttribute('name', name)
+    component.setAttribute('path', path)
     component.setAttribute('data', JSON.stringify(data))
     return component
   }
 
   connectedCallback() {
-    const nameValue = this.getAttribute('name')
+    this._parseName()
+    if (!this.name) { this.innerHTML = 'Missing name attribute'; return }
     this.data = JSON.parse(this.getAttribute('data'))
-    if (!nameValue) {
-      this.innerHTML = 'Missing name attribute'
-      return
-    }
-    fetch(`./components/${nameValue}.html`)
-      .then(response => response.text())
-      .then(html => {
 
-        // update html
+    fetch(`components/${this.path}.html`).then(res => res.text()).then(html => {
         this.innerHTML = html;
 
         // rerun css with 'this' shortcut replaced
         this.querySelectorAll('style').forEach(style => {
-          style.textContent = style.textContent.replace(/this/g, `${Component.tag}[name=${nameValue}]`)
+          style.textContent = style.textContent.replace(/this/g, this.query())
         })
 
         // run scripts manualy as they dont auto run
         this.querySelectorAll('script').forEach(script => {
           script.text = script.text.replace(/this/g, 'self') // replace 'this' with 'self'
           const newScript = document.createElement('script')
-          newScript.text = `// components/${nameValue}.html (data: ${this.data})\n`
+          newScript.text = `// ${this.path} (data: ${this.data})\n`
           newScript.text += `function invokeFillComponent(self) {${script.text}}`
           script.parentNode.replaceChild(newScript, script)
           invokeFillComponent(this) // call with 'this'
@@ -38,8 +32,21 @@ class Component extends HTMLElement {
       })
   }
 
-  static find(name) { 
-    return document.querySelector(`${Component.tag}[name=${name}]`) 
+  query() { return Component._query(this.path) }
+
+  static _query(path) {
+    return `${Component.tag}[name=${path.replace('/', '-')}]`
+  }
+
+  static find(path) { 
+    return document.querySelector(Component._query(path)) 
+  }
+
+  _parseName() {
+    this.path = this.getAttribute('path')
+    const parts = this.path.split('/')
+    this.setAttribute('name', parts.join('-'))
+    this.name = parts.pop()
   }
 }
 
